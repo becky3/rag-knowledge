9d105f360052393b7e0dbf4b8f590c88fae1ad9d
becky <satoru-kibe@rhythmcan.com>
2026-03-11 09:08:46 +0900

    Revert "Merge pull request #74 from becky3/auto/issue-71-20260310-2247"
    This reverts commit 4fc03a0fa0d6cecb672dd043955b50362025c2f4, reversing
    changes made to 6b47d31c859e49b6fa7cc201f5f70e2a9d9ed74f.

--- src/rag/ingesters/__init__.py
+++ src/rag/ingesters/__init__.py
@@ -5,12 +5,10 @@ Issue: #62
 """
 
 from .base import BaseIngester, IngestedContent
-from .local_file import LocalFileIngester
 from .web import WebIngester
 
 __all__ = [
     "BaseIngester",
     "IngestedContent",
-    "LocalFileIngester",
     "WebIngester",
 ]
